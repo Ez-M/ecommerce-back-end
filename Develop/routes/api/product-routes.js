842d881aede662e6e19be3c83bd5194f76e111a5
Ezequiel <martinez.ezequiel42@gmail.com>
2021-09-24 18:39:38 -0700

    added .get/ and .get/:id to product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,12 +6,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+    // find all associated tags from ProductTag
+    return Product.findAll();
+
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  return Product.findAll({ where: { product_id: req.params.id } });
   // be sure to include its associated Category and Tag data
 });
 
